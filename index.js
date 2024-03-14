e3d1171dc04996a2ed2d9a50811e3b0037554c3f
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-14 22:48:10 +0200

    Implement mongoose

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from "express";
 import cors from "cors";
 
+const { initializeMongoose } = require("./database");
+const router = express.Router();
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my server!");
-});
+initializeMongoose();
 
-app.use(cors());
+app.use(cors(), router);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
