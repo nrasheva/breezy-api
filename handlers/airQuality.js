f7597ebeb81e7a1193da53bac57f6f5b15715f56
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-17 23:19:45 +0200

    Code refining

--- handlers/airQuality.js
+++ handlers/airQuality.js
@@ -13,7 +13,7 @@ async function getAirQuality(req, res) {
 
   try {
     const airQuality = await axios.get(
-      `https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${lng}&current=european_aqi,pm10,pm2_5,carbon_monoxide,dust,uv_index,alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=european_aqi,european_aqi_pm2_5,european_aqi_pm10&timeformat=unixtime&timezone=auto`
+      `/v1/air-quality?latitude=${lat}&longitude=${lng}&current=european_aqi,pm10,pm2_5,carbon_monoxide,dust,uv_index,alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=european_aqi,european_aqi_pm2_5,european_aqi_pm10&timeformat=unixtime&timezone=auto`
     );
 
     const response = {
