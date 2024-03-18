1c5d959e284340fe9f861e97b76455771a8a2e55
Nadezhda Rasheva <nadezhda.rasheva96@gmail.com>
2024-03-18 23:52:51 +0200

    Update getAirQuality handler

--- handlers/airQuality.js
+++ handlers/airQuality.js
@@ -3,24 +3,20 @@ const { axios } = require("../axios");
 async function getAirQuality(req, res) {
   const { lat, lng } = req.query;
 
-  if (!lat) {
-    res.status(400).json({ message: "latitude parameter not provided" });
-    return;
-  } else if (!lng) {
-    res.status(400).json({ message: "longitude parameter not provided" });
+  if (!lat || !lng) {
+    res
+      .status(400)
+      .json({ message: "Latitude and/or longitude parameter not provided" });
     return;
   }
 
+  const baseURL = process.env.AIR_BASE_URL;
+  const requestURL = `${baseURL}/v1/air-quality?latitude=${lat}&longitude=${lng}&current=european_aqi,pm10,pm2_5,carbon_monoxide,dust,uv_index,alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=european_aqi,european_aqi_pm2_5,european_aqi_pm10&timeformat=unixtime&timezone=auto`;
+
   try {
-    const airQuality = await axios.get(
-      `/v1/air-quality?latitude=${lat}&longitude=${lng}&current=european_aqi,pm10,pm2_5,carbon_monoxide,dust,uv_index,alder_pollen,birch_pollen,grass_pollen,mugwort_pollen,olive_pollen,ragweed_pollen&hourly=european_aqi,european_aqi_pm2_5,european_aqi_pm10&timeformat=unixtime&timezone=auto`
-    );
+    const response = await axios.get(requestURL);
 
-    const response = {
-      airQuality: airQuality,
-    };
-
-    res.send(response);
+    res.json({ airQuality: response.data });
   } catch (error) {
     res
       .status(
